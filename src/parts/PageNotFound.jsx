0b09961e3ac9fc76a76c656bed0cbf6b4c2e950b
Karol Faltyn <karolfaltyn03@gmail.com>
2024-03-20 14:38:27 +0100

    styled error page

--- src/parts/PageNotFound.jsx
+++ src/parts/PageNotFound.jsx
@@ -2,13 +2,25 @@ import "../assets/style/style.css";
 import { Outlet, Link } from "react-router-dom";
 
 export const PageNotFound = () => {
+  const hero_img = require("../assets/images/on-pages/coffee-sketch.png");
+
   return (
     <section className="flex items-center">
       <div className="container">
         <div className="flex flex-col items-center justify-center">
-          <h1>404</h1>
-          <p>We don't have that coffee yet.</p>
-          <Link to="/">Let's go back.</Link>
+          <img
+            src={hero_img}
+            alt="coffee"
+            className="transition-all hover:scale-110"
+          />
+          <h1 className="text-5xl font-semibold">404</h1>
+          <p className="text-2xl font-medium">We don't have that coffee yet.</p>
+          <Link
+            to="/"
+            className="font-medium text-orange-400 underline decoration-wavy hover:opacity-70"
+          >
+            Let's go back.
+          </Link>
         </div>
       </div>
       <Outlet />

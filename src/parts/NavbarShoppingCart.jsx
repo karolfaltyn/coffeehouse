755956fa2b740bf84acc4130446ca1bcbeaf7a74
Karol Faltyn <karolfaltyn03@gmail.com>
2024-03-21 17:16:08 +0100

    fixed navbar cart issue

--- src/parts/NavbarShoppingCart.jsx
+++ src/parts/NavbarShoppingCart.jsx
@@ -5,20 +5,29 @@ import "../assets/style/style.css";
 export const NavbarShoppingCart = ({ toggleCart, isCartOpen }) => {
   const coffee_svg = require("../assets/images/svg/bx-coffee.svg").default;
   const cart = require("../assets/images/svg/bx-cart.svg").default;
-  const close = require("../assets/images/svg/bx-x.svg").default;
 
   return (
     <nav className="fixed inset-x-0 top-0 bg-[#F3f3f3] font-semibold">
       <div className="container">
         <div className="flex items-center justify-between py-6">
           <div>
-            <Link to="/" className="flex gap-2 font-semibold hover:opacity-70">
-              COFFEEHOUSE
-              <img src={coffee_svg} alt="logo" className="sm:hidden" />
-            </Link>
+            {isCartOpen ? (
+              <span className="flex gap-2 font-semibold">
+                COFFEEHOUSE
+                <img src={coffee_svg} alt="logo" className="sm:hidden" />
+              </span>
+            ) : (
+              <Link
+                to="/"
+                className="flex gap-2 font-semibold hover:opacity-70"
+              >
+                COFFEEHOUSE
+                <img src={coffee_svg} alt="logo" className="sm:hidden" />
+              </Link>
+            )}
           </div>
-          <button onClick={toggleCart}>
-            <img src={isCartOpen ? close : cart} alt="shopping cart" />
+          <button onClick={toggleCart} disabled={isCartOpen}>
+            <img src={cart} alt="shopping cart" />
           </button>
         </div>
       </div>
